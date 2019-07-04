6a2d368d5682c54dcfd975bc52f2f996f529d16f
vishalcoderathore <vshl.rathore87@gmail.com>
2019-07-04 11:36:35 -0500

    Implemented destroy functionality for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,17 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
     end
 
+    def destroy
+        # This will perform the lookup
+        @portfolio_item = Portfolio.find(params[:id])
+
+        # Destroy/delete the record
+        @portfolio_item.destroy
+
+        # Redirect
+        respond_to do |format|
+            format.html {redirect_to portfolios_url, notice: "Success : Portfolio is deleted."}
+        end
+    end
+
 end
